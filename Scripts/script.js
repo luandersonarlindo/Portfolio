274ed1638bd66b74c380eb0918c4880f8276d66b
luandersonarlindo <luan.arlindo20166@gmail.com>
2024-07-25 21:56:07 -0300

    Criação de slideImage completo

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,79 +1,29 @@
-var nav = window.document.getElementById('nav')
-nav.innerHTML = `
-<div class="menu-burger">
-</div>
 
-<ul class="nav-long">
-    <li><a href="#">Sobre Mim</a></li>
-    <li><a href="#">Projetos</a></li>
-    <li><a href="#">conhecimentos</a></li>
-    <li><a href="#">Fale comigo</a></li>
-</ul>
-`
+/* Nav */
 
+/* Intro */
 
-var intro = window.document.getElementById('intro')
-intro.innerHTML = `
-<div class="nome">
-    <h1>Luanderson Arlindo</h1>
-    <h2>Desenvolvedor Web</h2>
-</div>
-<div class="programmer">
-</div>
-`
+/* Sobre mim */
 
-var sobreMim = window.document.getElementById('sobreMim')
-sobreMim.innerHTML = `
-<div class="foto-perfil">
-</div>
-<div class="sobre-mim-texto">
-    <h3>Sobre mim</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, ex quae, non nam
-        repudiandae
-        eaque adipisci commodi, dignissimos maiores quo veritatis architecto veniam. Mollitia maxime
-        doloremque ducimus natus molestiae laudantium!
-        Rerum magni reprehenderit, laboriosam quod nulla amet debitis modi animi? Ipsum molestiae ut
-        omnis veritatis facere nostrum magni asperiores laudantium tempora ipsam ducimus velit
-        quisquam,
-        illo doloremque. Dolorum, sint natus.
-    </p>
-</div>
-`
+/* Projeto */
 
-var projeto = window.document.getElementById('projeto')
-projeto.innerHTML = `
-<h3>Projeto</h3>
+let count = 1;
+document.getElementById('radio1').checked = true;
 
-<div class="cards">
-    <div class="card">
-        <div class="card2">
-        </div>
-        <div class="card-balls">
-            <div class="balls01"></div>
-            <div class="balls02"></div>
-            <div class="balls03"></div>
-            <div class="balls04"></div>
-            <div class="balls05"></div>
-        </div>
-    </div>
-    <div class="innovation">
-    </div>
-</div>
-`
+setInterval(function () {
+    nextImage()
+}, 5000)
 
-var conhecimento = window.document.getElementById('conhecimentos')
-conhecimento.innerHTML = `
-<h3>Conhecimentos</h3>
+function nextImage() {
+    count++;
+    if (count > 5) {
+        count = 1;
+    }
 
-<div class="conhecimentos">
-    <div class="operating">
-    </div>
-    <div class="conh">
-        <div class="con01"></div>
-        <div class="con02"></div>
-        <div class="con03"></div>
-        <div class="con04"></div>
-        <div class="con05"></div>
-    </div>
-</div>
-`
+    document.getElementById('radio' + count).checked = true;
+
+}
+
+/* Conhecimento */
+
+/* Fale comigo */
