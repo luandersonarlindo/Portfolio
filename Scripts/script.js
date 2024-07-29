e791e6580f1ee16fbed9b117e7ef93a60ca7a3b7
luandersonarlindo <luan.arlindo20166@gmail.com>
2024-07-29 12:02:44 -0300

    favicon add

--- Scripts/script.js
+++ Scripts/script.js
@@ -27,3 +27,59 @@ function nextImage() {
 /* Conhecimento */
 
 /* Fale comigo */
+
+
+// Objeto
+const contato = {
+    email: "doninhanada@gmail.com",
+    nome: "doninhaNada",
+    mensagem: "doninha",
+    finalizada: true
+}
+
+
+// Lita
+
+let contatos = [
+    contato, {
+        email: "Nada1990@gmail.com",
+        nome: "nada1990",
+        mensagem: "nada",
+        finalizada: true
+    }, {
+        email: "cara123@gmail.com",
+        nome: "cara123",
+        mensagem: "cara",
+        finalizada: true
+    }
+]
+
+atualizarListaDeContatos()
+
+// Salva conato
+
+const salvaContato = (event) => {
+    event.preventDefault()
+    const dadosDoFormulario = new FormData(event.target)
+
+    const email = dadosDoFormulario.get('email')
+    const nome = dadosDoFormulario.get('nome')
+    const mensagem = dadosDoFormulario.get('mensagem')
+
+    const novoForm = {
+        email,
+        nome,
+        mensagem: false
+    }
+
+    const contatoExiste  = contatos.find((contato) => {
+        return contato.email == novoForm.email
+    })
+
+    if (contatoExiste) {
+        return alert('Contato já existe')
+    }
+
+    contatos = [novoForm, ...contatos]
+    atualizarListaDeContatos()
+}
